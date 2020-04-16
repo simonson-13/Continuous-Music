cb440cdc2d39365711def2e9c428b62137d3dfb4
Shivam Patel <shivampatel623@gmail.com>
2020-04-16 17:27:55 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import InstrumentSelection from './InstrumentSelection/InstrumentSelection.js'
+import Visualization from './Visualization/Visualization.js'
+import Toolbar from './Toolbar/Toolbar.js'
+
+/* import {
+  Switch,
+  Paper, 
+  Slide,
+  FormControlLabel
+  } from '@material-ui/core'; */
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      instrument: "" // will be set by instrument selection component later
+    }
+  }
+
+  /*
+    TODO: keyboard bindings 
+    A-L (or so) = musical sounds
+    R = rerecord?
+    space = record
+    i = opens up 'info page' (along with mouse click) 
+    **potential for other keyboard bindings!
+  */
+
+
+  render() {
+    return (
+      <div className="App">
+        <Visualization />
+
+        <Toolbar />
+      </div>
+    );
+  }
 }
 
 export default App;
