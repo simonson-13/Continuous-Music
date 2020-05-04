7e56954d2b282cde9ac2361dad948499c03aa9b0
Shannen Lam <shannenlm@gmail.com>
2020-05-04 02:15:08 -0700

    got rid of some compilation warnings

--- src/Visualization/Visualization.js
+++ src/Visualization/Visualization.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {
     IconButton,
-    Tooltip 
+    Tooltip
 } from '@material-ui/core';
 
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
@@ -18,7 +18,7 @@ import './Visualization.css';
 const useStyles = makeStyles((theme) => ({
     root: {
         position: "relative",
-        justifyContent:"center",
+        justifyContent: "center",
         height: "100vh",
         color: "white"
     },
@@ -39,58 +39,46 @@ export default function Visualization(props) {
 
     return (
         <div className={classes.root}>
-            <Tooltip title="Info" arrow> 
-                <IconButton 
-                    className={classes.openButton} 
-                    type="button" 
+            <Tooltip title="Info" arrow>
+                <IconButton
+                    className={classes.openButton}
+                    type="button"
                     onClick={props.openInfo}
                 >
                     <HelpOutlineIcon />
                 </IconButton>
             </Tooltip>
 
-            <br/>
-            <Tooltip title="Chat" arrow> 
-                <IconButton 
-                    className={classes.openButton} 
-                    type="button" 
+            <br />
+            <Tooltip title="Chat" arrow>
+                <IconButton
+                    className={classes.openButton}
+                    type="button"
                     onClick={props.handleChatChange}
                 >
                     <QuestionAnswerIcon />
                 </IconButton>
             </Tooltip>
 
-            <br/><br/>&nbsp;Users:&nbsp;{props.userCount}
+            <br /><br />&nbsp;Users:&nbsp;{props.userCount}
 
-            {/* TODO:
-                p5.js Sketch as Background tutorial: 
-                    https://www.youtube.com/watch?v=OIfEHD3KqCg
-
-                    tl;dr -- give the canvas a style of z-index of -1 in setup():
-                        canvas = createCanvas(windowWidth, windowHeight);
-                        canvas.position(0, 0);
-                        canvas.style('z-index', '-1');
-                potentially helpful: 
-                    https://stackoverflow.com/questions/54868777/how-to-use-react-with-p5-js
-            */}
             <DimensionsProvider>
-                  {({ containerWidth, containerHeight}) => (
-                      <Sketch 
+                {({ containerWidth, containerHeight }) => (
+                    <Sketch
                         width={containerWidth}
                         height={containerHeight}
-                      />
-                  )}
-                </DimensionsProvider>
-            
+                    />
+                )}
+            </DimensionsProvider>
 
-            <div className={classes.openDrop}> 
-                <Tooltip title="Open Toolbar" arrow> 
-                    <IconButton 
-                        className={classes.openButton} 
+            <div className={classes.openDrop}>
+                <Tooltip title="Open Toolbar" arrow>
+                    <IconButton
+                        className={classes.openButton}
                         onClick={props.handleBarChange}
-                    > 
-                        <KeyboardArrowUpIcon/>
-                    </IconButton> 
+                    >
+                        <KeyboardArrowUpIcon />
+                    </IconButton>
                 </Tooltip>
             </div>
         </div>
--- src/ophir_piano/PianoApp.js
+++ src/ophir_piano/PianoApp.js
@@ -4,29 +4,8 @@ import 'react-piano/dist/styles.css';
 import InteractiveDemoFireBase from './InteractiveDemoFireBase.js';
 import './PianoApp.css';
 
-import _ from 'lodash';
-import { KeyboardShortcuts, MidiNumbers } from 'react-piano';
-
-// import SoundfontProvider from './SoundfontProvider';
-// import PianoWithRecording from './PianoWithRecording';
-// import PlaybackDemo from './PlaybackDemo';
-// import InteractiveDemo from './InteractiveDemo';
-
-// import * as firebase from 'firebase'; // import firebase!
-
-// webkitAudioContext fallback needed to support Safari
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 const soundfontHostname = 'https://d1pzp51pvbm36p.cloudfront.net';
-
-const noteRange = {
-  first: MidiNumbers.fromNote('c3'),
-  last: MidiNumbers.fromNote('f4'),
-};
-const keyboardShortcuts = KeyboardShortcuts.create({
-  firstNote: noteRange.first,
-  lastNote: noteRange.last,
-  keyboardConfig: KeyboardShortcuts.HOME_ROW,
-});
 
 class PianoApp extends Component {
   render() {
